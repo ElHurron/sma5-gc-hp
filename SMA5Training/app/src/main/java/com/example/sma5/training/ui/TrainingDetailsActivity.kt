6aff4c15546023db60ba8a188d4c2f171a863281
ChiaraGatt <s2110307058@fhooe.at>
2024-01-12 09:16:19 +0100

    add layout and activity for details

---
+++ SMA5Training/app/src/main/java/com/example/sma5/training/ui/TrainingDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sma5.training.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sma5.training.R
+
+class TrainingDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training_details)
+    }
+}
