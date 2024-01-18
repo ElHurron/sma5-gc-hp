f85d680478cc4cf428c10cb7d970bea200fbe990
ChiaraGatt <s2110307058@fhooe.at>
2024-01-18 14:36:14 +0100

    add new layout views

---
+++ SMA5Training/app/src/main/java/com/example/sma5/training/activity_training_details_trainer.kt
@@ -0,0 +1,11 @@
+package com.example.sma5.training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_training_details_trainer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training_details_trainer)
+    }
+}
---
+++ SMA5Training/app/src/main/java/com/example/sma5/training/ui/EditTrainingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sma5.training.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sma5.training.R
+
+class EditTrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_training)
+    }
+}
